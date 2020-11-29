b4515213d7869dfdfa8c594248e7f6293c73cc80
Assissian <2200876452@qq.com>
2020-11-29 12:29:58 +0800

    my_section

---
+++ JavaScript/node/JS/Lv0.js
@@ -0,0 +1,14 @@
+﻿const person = {
+    name: "Bob",
+    age: "20",
+    address: {
+        city: "chongqing",
+        area: "Nanan"
+    }
+}
+
+let [a, b, c, d] = [person.name, person.age, person.address.city, person.address.area];
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
